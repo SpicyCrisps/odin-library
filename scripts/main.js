f67a9997cd8a0ed18b46d680a4a4ea390ba1915b
Spicy Crisps <spicycrisps@protonmail.com>
2024-06-01 20:23:02 -0400

    Add books to visual library, update read status, test prompt function

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 const MyLibrary = [];
 
+const body = document.body;
+const visualLibraryContainer = body.querySelector("#container")
+
 function Book(author,title,numberOfPages,readStatus)
 {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
-    this.readStatus = readStatus;
+    this.readStatus = Boolean(readStatus).valueOf();
     this.info = function()
     {
         const infoarray = [this.author, this.title, this.numberOfPages, this.readStatus];
@@ -20,5 +23,56 @@ function addBookToLibrary(author, title, numberOfPages, readStatus)
         console.log(MyLibrary)
 
 }
-addBookToLibrary("Author Test","Title Test","Pages Test","Status Test");
-addBookToLibrary("Author Test 2","Title Test 2","Pages Test 2","Status Test 2");
+
+addBookToLibrary("The Guy","A really bad good book","200",true);
+addBookToLibrary("The Guy 2","WEEE","2440",false);
+
+function updateVisualLibrary()
+{
+    for(let i = 0; i < MyLibrary.length; i++)
+        {
+            let visualBook = document.createElement("div");
+
+            visualBook.className =("book");
+            
+            visualBook.id = (Number(i)); //Used for unique specific changes
+
+            let author = MyLibrary[visualBook.id].author
+            let title = MyLibrary[visualBook.id].title
+            let numberOfPages = MyLibrary[visualBook.id].numberOfPages
+            let readStatus = MyLibrary[visualBook.id].readStatus
+
+            visualBook.addEventListener( "click", () => presentInfo(author,title,numberOfPages,readStatus) )
+            visualBook.addEventListener( "click", () => updateReadStatus(MyLibrary[visualBook.id]) )
+            
+            if (visualBook.id < 1)
+                {
+                    visualBook.style.backgroundColor="blue";
+                }
+            
+            visualLibraryContainer.appendChild(visualBook);
+        }
+}
+
+function updateReadStatus(bookIndex)
+{       
+    if(bookIndex.readStatus == true)
+    {
+        console.log("READ IT")
+        bookIndex.readStatus = false;
+        
+    }
+    else
+    {
+        console.log("DID NOT READ IT")
+        bookIndex.readStatus = true;
+    }
+}
+
+//Function name is not good
+function presentInfo(author,title,numberOfPages,readStatus)
+{
+    alert("The Author is: " + author + "\nThe Title is: " + title + "\n" + "You have Read: " + numberOfPages + " Pages \n" + "and have " + readStatus + " read it before")
+}
+
+updateVisualLibrary()
